91cd40b3520ee8f4313b59d0b5b53e73e64080c5
Mostafizur <mostafizur.cse@gmail.com>
2019-03-31 02:20:09 +0600

    recorder completed

--- MetalCameraRenderer/MetalCameraRenderer/CameraViewController.swift
+++ MetalCameraRenderer/MetalCameraRenderer/CameraViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import Metal
 import CoreMedia
 import CoreImage
+import AVFoundation
+
+
 
 enum CaptureType {
     case cameraFeed
@@ -20,10 +23,16 @@ enum CaptureType {
 internal final class CameraViewController: MTKViewController {
     @IBOutlet weak var heightLayout: NSLayoutConstraint!
     
+    let themeColor = UIColor.init(rgb: 0xFF0066)
     @IBOutlet weak var cameraCaptureButton: UIButton!
     @IBOutlet weak var effectCaptureButton: UIButton!
     @IBOutlet weak var stopCaptureButton: UIButton!
     
+    var timeMin = 0
+    var timeSec = 0
+    weak var timer: Timer?
+    var yourLabel:UILabel!
+    var recLayer:CALayer!
     var selectedIdf = "NM0"
     var displayTime:CMTime!
     var metalCameraSession: MetalCameraSession!
@@ -45,9 +54,17 @@ internal final class CameraViewController: MTKViewController {
         self.cellheight = self.heightLayout.constant
     }
     
+    @IBOutlet weak var filterCollection: UICollectionView!
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-       
+        DispatchQueue.main.async {
+            self.filterCollection.reloadData()
+            self.effectCaptureButton.layer.cornerRadius = self.effectCaptureButton.frame.height / 2
+            self.effectCaptureButton.layer.masksToBounds = true
+            self.stopCaptureButton.layer.cornerRadius = self.stopCaptureButton.frame.height / 2
+            self.stopCaptureButton.layer.masksToBounds = true
+            
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -63,6 +80,68 @@ internal final class CameraViewController: MTKViewController {
         
     }
 
+    @IBAction func switchCamera(_ sender: Any) {
+        //Change camera source
+        if let ml = metalCameraSession {
+            let session = ml.captureSession
+            //Indicate that some changes will be made to the session
+            session.beginConfiguration()
+            
+            //Remove existing input
+            guard let currentCameraInput: AVCaptureInput = session.inputs.first else {
+                return
+            }
+            
+            session.removeInput(currentCameraInput)
+            
+            //Get new input
+            var newCamera: AVCaptureDevice! = nil
+            if let input = currentCameraInput as? AVCaptureDeviceInput {
+                if (input.device.position == .back) {
+                    newCamera = cameraWithPosition(position: .front)
+                } else {
+                    newCamera = cameraWithPosition(position: .back)
+                }
+            }
+            
+            //Add input to session
+            var err: NSError?
+            var newVideoInput: AVCaptureDeviceInput!
+            do {
+                newVideoInput = try AVCaptureDeviceInput(device: newCamera)
+            } catch let err1 as NSError {
+                err = err1
+                newVideoInput = nil
+            }
+            
+            if newVideoInput == nil || err != nil {
+                print("Error creating capture device input: \(err?.localizedDescription)")
+            } else {
+                session.addInput(newVideoInput)
+            }
+            
+            //Commit all the configuration changes at once
+            session.commitConfiguration()
+        }
+    }
+    
+    // Find a camera with the specified AVCaptureDevicePosition, returning nil if one is not found
+    func cameraWithPosition(position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        if #available(iOS 10.0, *) {
+            let discoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+            for device in discoverySession.devices {
+                if device.position == position {
+                    return device
+                }
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        
+        
+        return nil
+    }
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         if self.metalCameraSession.captureSession.isRunning {
@@ -79,17 +158,40 @@ internal final class CameraViewController: MTKViewController {
     @IBAction func startEffectCapturing(_ sender: UIButton) {
         self.changeButtonVisibility(true)
         self.captureType = .metalFeed
+        self.recLayer = CALayer()
+        self.recLayer.frame = CGRect(x: 22, y: UIScreen.main.bounds.height - 25, width: 22, height: 22)
+        self.recLayer.cornerRadius = 11
+        self.recLayer.masksToBounds = true
+        let colourAnim = CABasicAnimation(keyPath: "backgroundColor")
+        colourAnim.fromValue = UIColor.white.cgColor
+        colourAnim.toValue = UIColor.init(rgb: 0xFF0066).cgColor
+        colourAnim.duration = 1.0
+        colourAnim.autoreverses = true
+        colourAnim.repeatCount = Float.infinity
+        self.recLayer.add(colourAnim, forKey: "colourAnimation")
+        self.recLayer.backgroundColor = UIColor.init(rgb: 0xFF0066).cgColor
+        self.view.layer.insertSublayer(self.recLayer, at: 0)
+        
+        self.yourLabel = UILabel(frame: CGRect(x: 50, y: UIScreen.main.bounds.height - 25, width: 150, height: 22))
+        self.yourLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.light)
+        self.view.addSubview(self.yourLabel)
+        startTimer()
     }
-    
     
     @IBAction func finishCapturing(_ sender: UIButton) {
         if self.captureType == .metalFeed {
+            self.recLayer.removeAllAnimations()
+            self.recLayer.removeFromSuperlayer()
+            self.recLayer = nil
+            self.yourLabel.removeFromSuperview()
+            self.stopTimer()
             self.metalCameraSession.captureSessionQueueAudio.async {
                 self.metalWriteQueue.async {
                     self.shouldWritetexture = false
                     self.metalVideoWriter?.endVideoCapture()
                 }
             }
+            
         } else if self.captureType == .cameraFeed {
             self.cameraVideoWriter?.endVideoCapture()
         }
@@ -99,7 +201,7 @@ internal final class CameraViewController: MTKViewController {
     
     public func changeButtonVisibility(_ hidden:Bool){
         self.effectCaptureButton.isHidden = hidden
-        self.cameraCaptureButton.isHidden = hidden
+//        self.cameraCaptureButton.isHidden = hidden
         self.stopCaptureButton.isHidden = !hidden
         self.metalCameraSession.captureSessionQueueAudio.async {
             self.shouldWritetexture = true
@@ -117,6 +219,7 @@ internal final class CameraViewController: MTKViewController {
             }
         }
     }
+    
     
     override func writeVideo(Texture intexture: MTLTexture) {
         if self.captureType == .metalFeed {
@@ -136,6 +239,65 @@ internal final class CameraViewController: MTKViewController {
         }
     }
 
+    
+    @objc fileprivate func timerTick(){
+        timeSec += 1
+        
+        if timeSec == 60{
+            timeSec = 0
+            timeMin += 1
+        }
+        
+        let timeNow = String(format: "%02d:%02d", timeMin, timeSec)
+        
+        yourLabel.text = timeNow
+    }
+    
+    // resets both vars back to 0 and when the timer starts again it will start at 0
+    @objc fileprivate func resetTimerToZero(){
+        timeSec = 0
+        timeMin = 0
+        stopTimer()
+    }
+    
+    
+    
+    // if you need to reset the timer to 0 and yourLabel.txt back to 00:00
+    @objc fileprivate func resetTimerAndLabel(){
+        
+        resetTimerToZero()
+        yourLabel.text = String(format: "%02d:%02d", timeMin, timeSec)
+    }
+    
+    // stops the timer at it's current time
+    @objc fileprivate func stopTimer(){
+        
+        timer?.invalidate()
+    }
+    // MARK:- Timer Functions
+    fileprivate func startTimer(){
+        
+        // if you want the timer to reset to 0 every time the user presses record you can uncomment out either of these 2 lines
+        
+        timeSec = 0
+        timeMin = 0
+        
+        // If you don't use the 2 lines above then the timer will continue from whatever time it was stopped at
+        let timeNow = String(format: "%02d:%02d", timeMin, timeSec)
+        yourLabel.text = timeNow
+        
+        stopTimer() // stop it at it's current time before starting it again
+        if #available(iOS 10.0, *) {
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+                self?.timerTick()
+            }
+        } else {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTick), userInfo: nil, repeats: true)
+            
+            // Fallback on earlier versions
+        }
+        timer?.tolerance = 0.1
+    }
 }
 
 
@@ -185,6 +347,7 @@ extension CameraViewController: CameraSessionDelegate {
                             error: MetalCameraSessionError?) {
         
     }
+    
 }
 
 
@@ -218,25 +381,27 @@ extension CameraViewController : UICollectionViewDelegate, UICollectionViewDataS
         let filter = self.filterHelper.getFilter(At: indexPath.row)
         contentCell.effectImageView.image = UIImage(named: "icon")
 //        if contentCell.effectImageView.layer.cornerRadius == 0 {
-            contentCell.effectImageView.layer.cornerRadius = contentCell.effectImageView.frame.height / 2
+            contentCell.effectImageView.layer.cornerRadius = 35
             contentCell.effectImageView.layer.masksToBounds = true
 //        }
         if self.selectedIdf.elementsEqual(filter.filterID){
-            contentCell.layer.borderColor = UIColor.black.cgColor
-            contentCell.layer.borderWidth = 5
+            contentCell.effectImageView.layer.borderColor = self.themeColor.cgColor
+            contentCell.effectImageView.layer.borderWidth = 3
         } else {
-            contentCell.layer.borderWidth = 0
+            contentCell.effectImageView.layer.borderWidth = 0
         }
         contentCell.effectTitle.text = filter.title
         return contentCell
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        (cell as! EffectCell).effectImageView.layer.cornerRadius =  (cell as! EffectCell).effectImageView.frame.height / 2
+    }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: cellwidth - cellSpace * 2, height: self.cellheight - cellSpace * 2)
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return cellSpace
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -244,7 +409,7 @@ extension CameraViewController : UICollectionViewDelegate, UICollectionViewDataS
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return cellSpace
+        return 8
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -260,14 +425,31 @@ extension CameraViewController : UICollectionViewDelegate, UICollectionViewDataS
         }
         let _index = IndexPath(row: index, section: 0)
         if let cell = collectionView.cellForItem(at: _index) as? EffectCell {
-            cell.layer.borderWidth = 0
+            cell.effectImageView.layer.borderWidth = 0
         }
         self.selectedIdf = filter.filterID
         self.baseKernel = self.filterHelper.getKernel(At: indexPath.row, Deivce: self.metalData)
         
         let _cell = collectionView.cellForItem(at: indexPath) as! EffectCell
-        _cell.layer.borderColor = UIColor.black.cgColor
-        _cell.layer.borderWidth = 5;
+        _cell.effectImageView.layer.borderColor = self.themeColor.cgColor
+        _cell.effectImageView.layer.borderWidth = 3;
+        self.filterCollection.reloadData()
+    }
+}
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
         
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
     }
 }
--- MetalCameraRenderer/MetalCameraRenderer/VideoViewController.swift
+++ MetalCameraRenderer/MetalCameraRenderer/VideoViewController.swift
@@ -9,6 +9,9 @@
 
 import UIKit
 import AVFoundation
+import Photos
+
+
 class VideoViewController: UIViewController {
 
     @IBOutlet weak var backButton: UIButton!
@@ -29,6 +32,39 @@ class VideoViewController: UIViewController {
         super.viewDidAppear(animated)
         self.player.play()
         self.view.bringSubviewToFront(self.backButton)
+    }
+    
+    @IBAction func saveVideoData(_ sender: Any) {
+        self.saveVideo()
+    }
+    func saveVideo(){
+        PHPhotoLibrary.requestAuthorization { (authorizationStatus) in
+            switch authorizationStatus {
+            case .authorized :
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: self.videoURL)
+                }) { saved, error in
+                    if saved {
+                        let alertController = UIAlertController(title: "Your video was successfully saved", message: nil, preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+                break
+            case .notDetermined:
+                print("not determined")
+                break
+            case .restricted:
+                print("restricted")
+                break
+            case .denied:
+                let notificationName = Notification.Name(rawValue: "media_reading_denied")
+                NotificationCenter.default.post(name: notificationName, object: nil)
+                print("denied")
+                break
+            }
+        }
     }
     /*
     // MARK: - Navigation
